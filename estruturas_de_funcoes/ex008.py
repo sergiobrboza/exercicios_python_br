510486401836ba75305a2567d29730d144ac8910
Sérgio Barboza <98004964+sergiobrboza@users.noreply.github.com>
2023-06-11 13:16:29 -0300

    Exercicios funçoes

---
+++ estruturas_de_funcoes/ex008.py
@@ -0,0 +1,1 @@
+# 8.Faça uma função que informe a quantidade de dígitos de um determinado número inteiro informado.
---
+++ estruturas_de_funcoes/ex009.py
@@ -0,0 +1,1 @@
+# 9.Reverso do número. Faça uma função que retorne o reverso de um número inteiro informado. Por exemplo: 127 -> 721.
