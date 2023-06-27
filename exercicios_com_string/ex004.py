9b2a87f4e058abdddf5a209221ab1d9c9814a06a
Sérgio Barboza <98004964+sergiobrboza@users.noreply.github.com>
2023-06-27 12:49:30 -0300

    Exercicios

---
+++ exercicios_com_string/ex004.py
@@ -0,0 +1,8 @@
+# 4.Nome na vertical em escada. Modifique o programa anterior de forma a mostrar o nome em formato de escada.
+
+# F
+# FU
+# FUL
+# FULA
+# FULAN
+# FULANO
---
+++ exercicios_com_string/ex007.py
@@ -0,0 +1,4 @@
+# 7.Conta espaços e vogais. Dado uma string com uma frase informada pelo usuário (incluindo espaços em branco), conte:
+
+# A.quantos espaços em branco existem na frase.
+# B.quantas vezes aparecem as vogais a, e, i, o, u.
